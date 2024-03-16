e7b4bdc07fc42941853739f4f9673b5de034d5f6
Garci232 <158181887+Garci232@users.noreply.github.com>
2024-03-16 02:56:13 -0500

    Create convert_temperature_from_command_line.py

---
+++ user_input_and_error_handling/convert_temperature_from_command_line.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def convert_fahrenheit_to_celsius(fahrenheit_temperature):
+    """Convert input temperature in degrees Fahrenheit to degrees Celsius."""
+    return (5.0/9) * (fahrenheit_temperature - 32)
+if __name__ == "__main__":
+    try:
+        if len(sys.argv) < 2:
+            raise ValueError("Fahrenheit temperature missing on the command line.")
+
+        fahrenheit = float(sys.argv[1])
+        celsius = convert_fahrenheit_to_celsius(fahrenheit)
+        print(f"Temperature in Fahrenheit: {fahrenheit}°F")
+        print(f"Temperature in Celsius: {celsius}°C")
+    except ValueError as ve:
+        print(f"Error: {ve}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+Rerun
