af88035d3fda3b34efc97ddfe64bc2a75236a297
Garci232 <158181887+Garci232@users.noreply.github.com>
2024-03-16 02:54:12 -0500

    Create convert_fahrenheit_temperature_to_celsius_from_file.py

---
+++ user_input_and_error_handling/convert_fahrenheit_temperature_to_celsius_from_file.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def convert_fahrenheit_to_celsius(fahrenheit_temperature):
+  """Convert input temperature in degrees Fahrenheit to degrees Celsius."""
+  return (5.0/9) * (fahrenheit_temperature - 32)
+if __name__ == "__main__":
+  try:
+      with open('temperature_data.txt', 'r') as file:
+          content = file.readlines()
+          fahrenheit = float(content[3].split()[2])
+          celsius = convert_fahrenheit_to_celsius(fahrenheit)
+          print(f"Temperature in Fahrenheit: {fahrenheit}°F")
+          print(f"Temperature in Celsius: {celsius}°C")
+  except FileNotFoundError:
+      print("File not found.")
+  except Exception as e:
+      print(f"An error occurred: {e}")
