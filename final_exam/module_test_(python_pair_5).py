996074f16aff1c785f8779022c575f6163b75e43
Garci232 <158181887+Garci232@users.noreply.github.com>
2024-05-10 07:46:38 +0000

    updated final_exam

--- final_exam/module_test_(python_pair_5).py
+++ final_exam/module_test_(python_pair_5).py
@@ -1,13 +1,10 @@
-"""Execute each module which contains a standalone test in its if \
+"""
+Execute each module which contains a standalone test in its if \
 __name__ == "__main__": block
 
-__Nicolas Garcia__
+__author__ = "Nicolas Garcia"
 """
-
-
-modules = ['read_two_columns_text', 'calculate_bivariate_statistics', \
-           'calculate_quadratic_fit', 'fit_curve_array', 'plot_data_with_fit', \
-           'calculate_lowest_eigenvectors', 'annotate_plot']
+modules = ['read_two_columns_text', 'calculate_bivariate_statistics', 'calculate_quadratic_fit', 'fit_curve_array', 'plot_data_with_fit', 'calculate_lowest_eigenvectors', 'annotate_plot']
 for module in modules:
-    with open(module+'.py') as module_file:
+    with open(f'final_exam/{module}.py') as module_file:
         exec(module_file.read())
